348bed2501af95c6d73ba305c45dc1b3a3796b3d
zeyuyuyu <41896859+zeyuyuyu@users.noreply.github.com>
2025-01-17 23:46:42 -0800

    Create data.py

---
+++ data.py
@@ -0,0 +1,20 @@
+# data.py
+
+problems = [
+    {
+        "id": 1,
+        "symbolic": "x=2, y=3, 求 x+y 的值。",
+        "verbal": "假设x等于2，y等于3，求x加y的值是多少？"
+    },
+    {
+        "id": 2,
+        "symbolic": "a=5, b=10, 求 a*b 的值。",
+        "verbal": "假设a等于5，b等于10，求a乘以b的值是多少？"
+    },
+    {
+        "id": 3,
+        "symbolic": "m=7, n=4, 求 m-n 的值。",
+        "verbal": "假设m等于7，n等于4，求m减n的值是多少？"
+    },
+    # 可以根据需要添加更多问题
+]
