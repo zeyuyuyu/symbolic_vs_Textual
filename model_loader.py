29d528eedb767db302e57a2338d973873aaf1ca5
zeyuyuyu <41896859+zeyuyuyu@users.noreply.github.com>
2025-01-17 23:54:00 -0800

    Update model_loader.py

--- model_loader.py
+++ model_loader.py
@@ -1,11 +1,11 @@
 # model_loader.py
 
 import torch
-from transformers import T5Tokenizer, T5ForConditionalGeneration
+from transformers import T5Tokenizer, T5EncoderModel
 
 def load_model(model_name="t5-base", device=None):
     """
-    加载T5模型和分词器。
+    加载T5编码器模型和分词器。
 
     参数:
         model_name (str): 预训练模型名称，默认为't5-base'。
@@ -13,10 +13,11 @@ def load_model(model_name="t5-base", device=None):
 
     返回:
         tokenizer: T5分词器。
-        model: T5模型，设置为评估模式。
+        model: T5编码器模型，设置为评估模式。
+        device: 使用的计算设备。
     """
     tokenizer = T5Tokenizer.from_pretrained(model_name)
-    model = T5ForConditionalGeneration.from_pretrained(model_name, output_attentions=True)
+    model = T5EncoderModel.from_pretrained(model_name, output_attentions=True)
     
     if device:
         model.to(device)
@@ -26,3 +27,4 @@ def load_model(model_name="t5-base", device=None):
     
     model.eval()
     return tokenizer, model, device
+
